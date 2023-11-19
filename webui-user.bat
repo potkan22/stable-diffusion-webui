8f358e9996ec4fa630f4110b7a703799fcb9c26d
potkan22 <pokusnupotkan@gmail.com>
2023-11-19 19:44:31 +0100

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=c:\Users\zolo\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --api --xformers -- no half --disable-safe-unpickle --theme dark
+set COMMANDLINE_ARGS= --api
 
 call webui.bat
